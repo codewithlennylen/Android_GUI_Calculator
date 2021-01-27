fb04c85ee10f6022a24a8d7be2ddc66c0eefc070
Eng. Lenny Ng'ang'a <33583893+codewithlennylen@users.noreply.github.com>
2021-01-27 16:06:45 +0300

    Basic Dark Mode :+1:
    Still not Perfect :> Quite 'Blocky' and Traditional but Updates will be made soon:
    + Color Theme
    + Text Alignment
    +Rounded Buttons

--- Source_Code_Flutter/caclc/lib/main.dart
+++ Source_Code_Flutter/caclc/lib/main.dart
@@ -12,7 +12,7 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.blue[900],
+      backgroundColor: Colors.grey[900],
       appBar: AppBar(
         title: Text("Calculator"),
         centerTitle: true,
@@ -20,58 +20,357 @@ class Home extends StatelessWidget {
       ),
       body: Column(
         children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              FlatButton(
-                onPressed: null,
-                child: Text(
-                  "D",
-                  style: TextStyle(color: Colors.white),
+          // Theme Select
+          Container(
+            color: Colors.grey[700],
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                RaisedButton.icon(
+                  onPressed: null,
+                  icon: Icon(
+                    Icons.donut_large,
+                  ),
+                  label: Text(
+                    "D",
+                    style: TextStyle(color: Colors.grey[300]),
+                  ),
+                  color: Colors.red,
                 ),
-              ),
-              FlatButton(
-                onPressed: null,
-                child: Text(
-                  "L",
-                  style: TextStyle(color: Colors.white),
+
+                RaisedButton.icon(
+                  onPressed: null,
+                  icon: Icon(
+                    Icons.lightbulb_outline,
+                  ),
+                  label: Text(
+                    "L",
+                    style: TextStyle(color: Colors.grey[600]),
+                  ),
+                  color: Colors.red,
                 ),
-              ),
-            ],
+                // FlatButton(
+                //   onPressed: null,
+                //   child: Text(
+                //     "L",
+                //     style: TextStyle(color: Colors.white),
+                //   ),
+                // ),
+              ],
+            ),
           ),
+
+          // Whitespace
           Row(
             children: [
               SizedBox(
-                height: 20,
+                height: 210,
               )
             ],
           ),
+
+          // Formula and Answer Bar
           Container(
-            margin: EdgeInsets.fromLTRB(20, 5, 20, 0),
-            child: Row(
-              children: [Text("20 x 100")],
+            // alignment: ,
+            child: Text(
+              "20 x 30",
+              style: TextStyle(
+                color: Colors.white,
+                letterSpacing: 2,
+                fontSize: 35,
+              ),
             ),
           ),
           Container(
-            margin: EdgeInsets.fromLTRB(20, 5, 20, 0),
-            child: Row(
-              children: [Text("2000")],
+            // alignment: ,
+            child: Text(
+              "600",
+              style: TextStyle(
+                color: Colors.white,
+                letterSpacing: 2,
+                fontSize: 50,
+                fontWeight: FontWeight.bold,
+              ),
             ),
           ),
+
+          // Number Grid
           Container(
-            child: GridView.count(
-              crossAxisCount: 2,
-              crossAxisSpacing: 4.0,
-              mainAxisSpacing: 8.0,
+            color: Colors.grey[700],
+            child: Column(
+              // mainAxisAlignment: MainAxisAlignment.start,
+              // crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Text(
-                  "1",
-                  style: TextStyle(color: Colors.white),
+                // First Row
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    RaisedButton(
+                      onPressed: () {},
+                      child: Text(
+                        "AC",
+                        style: TextStyle(
+                          color: Colors.blueAccent[400],
+                          letterSpacing: 2,
+                          fontSize: 30,
+                        ),
+                      ),
+                      color: Colors.grey[900],
+                    ),
+                    RaisedButton(
+                      onPressed: () {},
+                      child: Text(
+                        "+",
+                        style: TextStyle(
+                          color: Colors.blueAccent[400],
+                          letterSpacing: 2,
+                          fontSize: 30,
+                        ),
+                      ),
+                      color: Colors.grey[900],
+                    ),
+                    RaisedButton(
+                      onPressed: () {},
+                      child: Text(
+                        "%",
+                        style: TextStyle(
+                          color: Colors.blueAccent[400],
+                          letterSpacing: 2,
+                          fontSize: 30,
+                        ),
+                      ),
+                      color: Colors.grey[900],
+                    ),
+                    RaisedButton(
+                      onPressed: () {},
+                      child: Text(
+                        "/",
+                        style: TextStyle(
+                          color: Colors.red,
+                          letterSpacing: 2,
+                          fontSize: 30,
+                        ),
+                      ),
+                      color: Colors.grey[900],
+                    ),
+                  ],
                 ),
-                Text(
-                  "2",
-                  style: TextStyle(color: Colors.white),
-                )
+                // Second Row
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    RaisedButton(
+                      onPressed: () {},
+                      child: Text(
+                        "7",
+                        style: TextStyle(
+                          color: Colors.white,
+                          letterSpacing: 2,
+                          fontSize: 30,
+                        ),
+                      ),
+                      color: Colors.grey[900],
+                    ),
+                    RaisedButton(
+                      onPressed: () {},
+                      child: Text(
+                        "8",
+                        style: TextStyle(
+                          color: Colors.white,
+                          letterSpacing: 2,
+                          fontSize: 30,
+                        ),
+                      ),
+                      color: Colors.grey[900],
+                    ),
+                    RaisedButton(
+                      onPressed: () {},
+                      child: Text(
+                        "9",
+                        style: TextStyle(
+                          color: Colors.white,
+                          letterSpacing: 2,
+                          fontSize: 30,
+                        ),
+                      ),
+                      color: Colors.grey[900],
+                    ),
+                    RaisedButton(
+                      onPressed: () {},
+                      child: Text(
+                        "X",
+                        style: TextStyle(
+                          color: Colors.red,
+                          letterSpacing: 2,
+                          fontSize: 30,
+                        ),
+                      ),
+                      color: Colors.grey[900],
+                    ),
+                  ],
+                ),
+                // Third Row
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    RaisedButton(
+                      onPressed: () {},
+                      child: Text(
+                        "4",
+                        style: TextStyle(
+                          color: Colors.white,
+                          letterSpacing: 2,
+                          fontSize: 30,
+                        ),
+                      ),
+                      color: Colors.grey[900],
+                    ),
+                    RaisedButton(
+                      onPressed: () {},
+                      child: Text(
+                        "5",
+                        style: TextStyle(
+                          color: Colors.white,
+                          letterSpacing: 2,
+                          fontSize: 30,
+                        ),
+                      ),
+                      color: Colors.grey[900],
+                    ),
+                    RaisedButton(
+                      onPressed: () {},
+                      child: Text(
+                        "6",
+                        style: TextStyle(
+                          color: Colors.white,
+                          letterSpacing: 2,
+                          fontSize: 30,
+                        ),
+                      ),
+                      color: Colors.grey[900],
+                    ),
+                    RaisedButton(
+                      onPressed: () {},
+                      child: Text(
+                        "-",
+                        style: TextStyle(
+                          color: Colors.red,
+                          letterSpacing: 2,
+                          fontSize: 30,
+                        ),
+                      ),
+                      color: Colors.grey[900],
+                    ),
+                  ],
+                ),
+                // Fourth Row
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    RaisedButton(
+                      onPressed: () {},
+                      child: Text(
+                        "1",
+                        style: TextStyle(
+                          color: Colors.white,
+                          letterSpacing: 2,
+                          fontSize: 30,
+                        ),
+                      ),
+                      color: Colors.grey[900],
+                    ),
+                    RaisedButton(
+                      onPressed: () {},
+                      child: Text(
+                        "2",
+                        style: TextStyle(
+                          color: Colors.white,
+                          letterSpacing: 2,
+                          fontSize: 30,
+                        ),
+                      ),
+                      color: Colors.grey[900],
+                    ),
+                    RaisedButton(
+                      onPressed: () {},
+                      child: Text(
+                        "3",
+                        style: TextStyle(
+                          color: Colors.white,
+                          letterSpacing: 2,
+                          fontSize: 30,
+                        ),
+                      ),
+                      color: Colors.grey[900],
+                    ),
+                    RaisedButton(
+                      onPressed: () {},
+                      child: Text(
+                        "+",
+                        style: TextStyle(
+                          color: Colors.red,
+                          letterSpacing: 2,
+                          fontSize: 30,
+                        ),
+                      ),
+                      color: Colors.grey[900],
+                    ),
+                  ],
+                ),
+                // Fifth Row
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    RaisedButton(
+                      onPressed: () {},
+                      child: Text(
+                        "!",
+                        style: TextStyle(
+                          color: Colors.white,
+                          letterSpacing: 2,
+                          fontSize: 30,
+                        ),
+                      ),
+                      color: Colors.grey[900],
+                    ),
+                    RaisedButton(
+                      onPressed: () {},
+                      child: Text(
+                        "0",
+                        style: TextStyle(
+                          color: Colors.white,
+                          letterSpacing: 2,
+                          fontSize: 30,
+                        ),
+                      ),
+                      color: Colors.grey[900],
+                    ),
+                    RaisedButton(
+                      onPressed: () {},
+                      child: Text(
+                        ".",
+                        style: TextStyle(
+                          color: Colors.white,
+                          letterSpacing: 2,
+                          fontSize: 30,
+                        ),
+                      ),
+                      color: Colors.grey[900],
+                    ),
+                    RaisedButton(
+                      onPressed: () {},
+                      child: Text(
+                        "=",
+                        style: TextStyle(
+                          color: Colors.red,
+                          letterSpacing: 2,
+                          fontSize: 30,
+                        ),
+                      ),
+                      color: Colors.grey[900],
+                    ),
+                  ],
+                ),
               ],
             ),
           )
